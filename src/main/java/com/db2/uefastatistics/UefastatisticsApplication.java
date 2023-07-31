304ded01391e6450b69f599b51cd43cb90b97a09
davideprezioso <d.prezioso3@studenti.unisa.it>
2023-07-31 16:38:30 +0200

    spring boot initializer added

---
+++ src/main/java/com/db2/uefastatistics/UefastatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.db2.uefastatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UefastatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UefastatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db2/uefastatistics/UefastatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db2.uefastatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UefastatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
